a1e3d94f64043145ac984e6b6beeb3bfa47de457
Zeyad-3naba <testtest157000@gmail.com>
2026-03-26 07:57:40 +0200

    Add eslint config

---
+++ eslint.config.js
@@ -0,0 +1,7 @@
+{
+  "files": ["node_modules"],
+  "rules": {
+    "react-refresh/only-export-components": ["warn", { "allowConstantExport": true }],
+    "@typescript-eslint/no-unused-vars": "warn"
+  }
+}
